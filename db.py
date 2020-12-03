9a3cc8f137b71740bdc73fc80a03743a6963ad4f
= <=>
2020-12-03 18:03:44 +0000

    basic stream scraper functionality

--- db.py
+++ db.py
@@ -1,25 +1,22 @@
 import sqlite3
 from sqlite3 import Error
 
+# Formatting helper functions
 def views_str_to_int(s):
+    """Input Examples: 
+        '100 viewers', '100,000 viewers', '100K viewers' '82.4K viewers'"""
     figure = s.split('viewers')[0]
-    return int(figure.replace(',', ''))
+    if 'K' in figure:
+        figure = figure.split('K')[0]
+        return int(float(figure)*1000)
+    else:
+        return int(figure.replace(',', ''))
+    
+def url_to_id(s, index):
+    return s.split('/')[index]
 
-def url_to_id(s):
-    return s.split('/')[-1]
-
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
-
+# SQL Statements
+# Create tables
 sql_create_categories_table = """
 CREATE TABLE IF NOT EXISTS categories (
     id text PRIMARY KEY,
@@ -33,6 +30,33 @@ CREATE TABLE IF NOT EXISTS cat_occurrences (
     cat_id text,
     FOREIGN KEY (cat_id) REFERENCES CATEGORIES (id)
 ); """
+sql_create_channels_table = """
+CREATE TABLE IF NOT EXISTS channels (
+    id text PRIMARY KEY,
+    name text NOT NULL
+); """
+sql_create_stream_table = """
+CREATE TABLE IF NOT EXISTS streams (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    seen_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    viewers int NOT NULL,
+    cat_id text,
+    channel_id text,
+    FOREIGN KEY (cat_id) REFERENCES CATEGORIES (id)
+    FOREIGN KEY (channel_id) REFERENCES CHANNELS (id)
+); """
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
 def create_connection(db_file):
     conn = None
@@ -43,20 +67,34 @@ def create_connection(db_file):
         print(e)
     return conn
 
+# Insert methods
 def insert_category(conn, url, title):
-    category = (url_to_id(url), title)
+    category = (url_to_id(url, -1), title)
     sql = ''' INSERT OR IGNORE INTO categories(id,title)
               VALUES(?,?) '''
     cur = conn.cursor()
     cur.execute(sql, category)
     conn.commit()
-
 def insert_cat_occurrence(conn, url, viewers):
-    occ = (views_str_to_int(viewers), url_to_id(url))
+    occ = (views_str_to_int(viewers), url_to_id(url, -1))
     sql = ''' INSERT OR IGNORE INTO cat_occurrences(viewers, cat_id)
               VALUES(?,?) '''
     cur = conn.cursor()
     cur.execute(sql, occ)
+    conn.commit()
+def insert_channel(conn, channel_url, channel_name):
+    channel = (url_to_id(channel_url, -2), channel_name)
+    sql = ''' INSERT OR IGNORE INTO channels(id,name)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, channel)
+    conn.commit()
+def insert_stream(conn, channel_url, cat_url, viewers):
+    stream = (views_str_to_int(viewers), url_to_id(cat_url, -1), url_to_id(channel_url, -2))
+    sql = ''' INSERT OR IGNORE INTO streams(viewers,cat_id,channel_id)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, stream)
     conn.commit()
 
 if __name__ == "__main__":
@@ -68,3 +106,9 @@ if __name__ == "__main__":
 
         # create cat_occurrences table
         create_table(conn, sql_create_cat_occurrences_table)
+
+        # create channels table
+        create_table(conn, sql_create_channels_table)
+
+        # create streams table
+        create_table(conn, sql_create_stream_table)
--- scraper.py
+++ scraper.py
@@ -2,26 +2,78 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-from db import create_connection, insert_category, insert_cat_occurrence
+from collections import deque
+from datetime import datetime
+import re
+from db import create_connection, insert_category, insert_cat_occurrence, insert_channel, insert_stream
 
+def get_valid_filename(s):
+    """Turn string into valid filename"""
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
+
+# Setting up
 conn = create_connection('example.db')
-
-url = 'https://www.twitch.tv/directory?sort=VIEWER_COUNT'
-
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
+
+# Scrape Directory page
+url = 'https://www.twitch.tv/directory?sort=VIEWER_COUNT'
 driver.get(url)
+# ToDo: If cookies button - click it
 driver.implicitly_wait(3)
 
 categories = driver.find_elements_by_class_name('tw-card')
-
+cats_q = deque() # want to scrape categories from left to right (so it looks nicer when taking screenshots), queue makes this efficient
 for cat in categories:
-    title = cat.find_element_by_tag_name('h3').text
-    viewers = cat.find_element_by_class_name('tw-c-text-alt-2').text
-    href = cat.find_element_by_tag_name('a').get_attribute('href')
+    #title = cat.find_element_by_tag_name('h3').text
+    body = cat.find_element_by_class_name('tw-card-body')
+    viewers = body.find_element_by_class_name('tw-c-text-alt-2').text
+    a_tag = body.find_element_by_tag_name('a')
+    href = a_tag.get_attribute('href')
+    title = a_tag.find_element_by_tag_name('h3').get_attribute('title')
+
+    # Add to DB
     insert_category(conn, href, title)
     insert_cat_occurrence(conn, href, viewers)
+
+    # Append to queue of categories for the next stage
+    print(title,href)
+    cats_q.append((title, href))
+
+print('Saving screenshot of Directory page')
+ts = datetime.now().strftime("%m-%dT%H%M%S")
+driver.save_screenshot(f'dir_page_{ts}.png')
+
+i = 0
+
+# Scrape Category pages
+while cats_q and i < 2:
+    title, cat_href = cats_q.popleft()
+    url = f'{cat_href}?sort=VIEWER_COUNT'
+    print('Scraping', title, url)
+    driver.get(url)
+    # ToDo: Set language (Just English for starters)
+    # ToDo: If cookies button - click it
+    driver.implicitly_wait(3)
+
+    streams = driver.find_elements_by_tag_name('article')
+    for stream in streams:
+        viewers = stream.find_element_by_class_name('tw-media-card-stat').text
+        channel_link = stream.find_element_by_xpath(".//a[@data-a-target='preview-card-channel-link']")
+        channel_href = channel_link.get_attribute('href')
+        channel_name = channel_link.text
+        print(viewers, channel_name, channel_href)
+
+        # Add to DB
+        insert_channel(conn, channel_href, channel_name)
+        insert_stream(conn, channel_href, cat_href, viewers)
     
-driver.save_screenshot('dir.png')
+    print(f'Saving screenshot of {title} page')
+    ts = datetime.now().strftime("%m-%dT%H%M%S")
+    driver.save_screenshot(f'{get_valid_filename(title)}_page_{ts}.png')
+    i += 1
+
+# Shutting down
 driver.close()
